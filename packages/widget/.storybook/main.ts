e2de0031b9fb3ad1ace0dcc07b5f58c81a8c5b1d
Audun Sørheim <audun.sorheim@nav.no>
2025-10-28 12:25:54 +0100

    refactor: Rename require to customRequire for clarity and update tsconfig to include exclude for .storybook

--- packages/widget/.storybook/main.ts
+++ packages/widget/.storybook/main.ts
@@ -2,20 +2,23 @@ import { createRequire } from "node:module";
 import { dirname, join } from "node:path";
 import type { StorybookConfig } from "@storybook/react-vite";
 
-const require = createRequire(import.meta.url);
+const customRequire = createRequire(import.meta.url);
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(ts|tsx)", "../src/**/*.mdx"],
-  addons: [getAbsolutePath("@storybook/addon-a11y"), getAbsolutePath("@storybook/addon-docs")],
+  addons: [
+    getAbsolutePath("@storybook/addon-a11y"),
+    getAbsolutePath("@storybook/addon-docs"),
+  ],
 
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
     options: {},
-  }
+  },
 };
 
 export default config;
 
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, "package.json")));
+  return dirname(customRequire.resolve(join(value, "package.json")));
 }
